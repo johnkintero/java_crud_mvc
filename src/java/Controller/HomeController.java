96596b555e8e285415e7f8d759b5950133d18f41
johnkintero <johnjairoqp@gmail.com>
2018-03-21 15:59:30 -0500

    Clase 6  21032018

--- src/java/Controller/HomeController.java
+++ src/java/Controller/HomeController.java
@@ -1,9 +1,11 @@
 
 package Controller;
 
+import Modelos.Persona;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
